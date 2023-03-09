40e9bdbbf7f8ab44dedd97e43df7612dbb8320f6
ancipet <as50674@fer.hr>
2023-03-09 17:09:26 +0100

    First homework

---
+++ hw01/dz.js
@@ -0,0 +1,35 @@
+function fullDecks (cards){
+    const cardsCount = {};
+
+    // create object with unique cards as keys, and number of repetitions as values
+    cards.forEach(element => {
+        cardsCount[element] = (cardsCount[element] || 0) + 1;
+    });
+
+    // get number of repetitions from object
+    const numOfRepeating = Object.values(cardsCount);
+
+    if (numOfRepeating.length == 52){
+        // number of possible decks is the same as the smallest number of repetition from any card
+        return numOfDecks = Math.min(...numOfRepeating)
+    } else {
+        return numOfDecks = 0;
+    }
+}
+
+// full deck, expected return value = 1
+//const cards = ['AC', 'AD', 'AH', 'AS', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', 'JC', 'JD', 'JH', 'JS', 'QC', 'QD', 'QH', 'QS', 'KC', 'KD', 'KH', 'KS'];
+
+// no cards, expected value = 0
+//const cards = []
+
+// 3 cards, expected value = 0
+//const cards = ['AC', 'AH', 'AS']
+
+// 1 full deck with duplicates od 'AC' and '2C' , expected value = 1
+const cards = ['AC', 'AC', 'AD', 'AH', 'AS', '2C', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', 'JC', 'JD', 'JH', 'JS', 'QC', 'QD', 'QH', 'QS', 'KC', 'KD', 'KH', 'KS'];
+
+// 2 full decks, expected value = 2
+//const cards = ['AC', 'AD', 'AH', 'AS', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', 'JC', 'JD', 'JH', 'JS', 'QC', 'QD', 'QH', 'QS', 'KC', 'KD', 'KH', 'KS', 'AC', 'AD', 'AH', 'AS', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', 'JC', 'JD', 'JH', 'JS', 'QC', 'QD', 'QH', 'QS', 'KC', 'KD', 'KH', 'KS'];
+
+console.log(fullDecks(cards));
